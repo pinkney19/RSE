70ac5649a33e33793b52bade50d14e16d5fc0dd8
Pinkney <pinkney@vdi-stori-029.lancs.local>
2024-10-30 14:09:29 +0000

    updates

--- 4_Synthetic_Experiments/Ridge/Tuning_results.R
+++ 4_Synthetic_Experiments/Ridge/Tuning_results.R
@@ -94,3 +94,82 @@ mc_lams = rbind(l12, l48, l96)
 print(ma_lams)
 print(mb_lams)
 print(mc_lams)
+
+
+
+# 50 Trials ---------------------------------------------------------------
+
+# Model a -----------------------------------------------------------------
+
+res1 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res1.RDS")
+res2 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res2.RDS")
+res3 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res3.RDS")
+
+# Recall structure of the files
+
+lambdas = logspace(-5,1,100)
+data_seq = seq(1, 10)
+grid = expand.grid(sample = data_seq, lams = lambdas)
+list_of_pairs <- split(grid, seq(nrow(grid)))
+N_samp = length(data_seq)
+P_vec = c(12,48,96)
+
+# Get ground truth --------------------------------------------------------
+
+ground_truth = Get_ground_truth(P_vec, 1, 0.0628)
+
+gt_12 = map(ground_truth,1)
+gt_48 = map(ground_truth,2)
+gt_96 = map(ground_truth,3)
+
+
+l12 = lam_func_ridge(lambdas, res1, N_samp, gt_12,12, 50)
+l48 = lam_func_ridge(lambdas, res2, N_samp, gt_48, 48, 50)
+l96 = lam_func_ridge(lambdas, res3, N_samp, gt_96, 96, 50)
+
+ma_lams = rbind(l12, l48, l96)
+
+# model b -----------------------------------------------------------------
+
+res4 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res4.RDS")
+res5 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res5.RDS")
+res6 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res6.RDS")
+
+
+ground_truth = Get_ground_truth(P_vec, 2, 0.0628)
+
+gt_12 = map(ground_truth,1)
+gt_48 = map(ground_truth,2)
+gt_96 = map(ground_truth,3)
+
+l12 = lam_func_ridge(lambdas, res4, N_samp, gt_12,12, 50)
+l48 = lam_func_ridge(lambdas, res5, N_samp, gt_48, 48, 50)
+l96 = lam_func_ridge(lambdas, res6, N_samp, gt_96, 96, 50)
+
+mb_lams = rbind(l12, l48, l96)
+
+
+# model c -----------------------------------------------------------------
+
+res7 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res7.RDS")
+res8 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res8.RDS")
+res9 <- readRDS("~/luna/Paper_Code/Section 4/Reviews/tuning/Ridge/50_trials/res9.RDS")
+
+
+ground_truth = Get_ground_truth(P_vec, 3, 0.0628)
+
+gt_12 = map(ground_truth,1)
+gt_48 = map(ground_truth,2)
+gt_96 = map(ground_truth,3)
+
+l12 = lam_func_ridge(lambdas, res7, N_samp, gt_12,12, 50)
+l48 = lam_func_ridge(lambdas, res8, N_samp, gt_48, 48, 50)
+l96 = lam_func_ridge(lambdas, res9, N_samp, gt_96, 96, 50)
+
+mc_lams = rbind(l12, l48, l96)
+
+#print all 
+
+print(ma_lams)
+print(mb_lams)
+print(mc_lams)
