bd08648aa72c3f1dfa711c7f79109a2f41be910c
Pinkney <pinkney@vdi-stori-029.lancs.local>
2025-02-04 08:27:36 +0000

    ups

--- 2_Preliminaries/Functions_Section_2.R
+++ 2_Preliminaries/Functions_Section_2.R
@@ -153,8 +153,7 @@ raw_periodogram = function(data, omega, Big_T){
   
   for(j in 1:m){
     for(i in 1:p){
-      #N_t[[j]][i] = length(data[[i]][[j]])
-      bar_d[[j]][i] = sum(exp(-img*omega*data[[i]][[j]]))  #- ((N_t[[j]][i]/Big_T) * H_omega )
+      bar_d[[j]][i] = sum(exp(-img*omega*data[[i]][[j]]))  
     }
     
     I[[j]] = (outer((bar_d[[j]]), Conj(bar_d[[j]])))/(2*pi)
