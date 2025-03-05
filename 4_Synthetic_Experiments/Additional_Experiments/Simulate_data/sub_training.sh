41ed4c1a976277822d3e2f3b5631ac293ba74dce
Pinkney <pinkney@vdi-stori-029.lancs.local>
2025-03-05 14:08:28 +0000

    addition_exp

---
+++ 4_Synthetic_Experiments/Additional_Experiments/Simulate_data/sub_training.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -J low_rank_train_data
+#SBATCH -c 21
+#SBATCH --array=1-2%2
+#SBATCH -e t%.e
+#SBATCH -o t%.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=c.pinkney@lancaster.ac.uk
+
+x=$SLURM_ARRAY_TASK_ID
+
+srun Rscript ~/RSE/4_Synthetic_exp/low_rank/Sim_Data/Simulating_Training_Data.R $x
